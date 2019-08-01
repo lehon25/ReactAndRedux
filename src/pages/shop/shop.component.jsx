1d9adb234f86275f0e1550af7736cdf6efb0d913
lehon aritonang <lehon_25@yahoo.com>
2019-08-02 05:23:52 +0700

    refactor collection to redux and bug fixing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { connect } from 'react-redux';
-import {createStructuredSelector } from 'reselect';
-import CollectionPreview from "../../components/collection-preview/collection-preview";
-import { selectCollections } from '../../redux/shop/shop.selector';
+import CollectionOverview from '../../components/collections-overview/collection-overview.component';
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionOverview/>
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections:selectCollections
-})
-export default connect (mapStateToProps)(ShopPage);
+export default ShopPage;
