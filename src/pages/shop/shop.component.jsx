6161358bc244885e30b0704545447d8e6570a6b5
lehon aritonang <lehon_25@yahoo.com>
2019-08-05 07:08:32 +0700

    create routing collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import CollectionOverview from '../../components/collections-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => {
+  console.log('ini math',match);
+  return (
   <div className="shop-page">
-    <CollectionOverview/>
+    <Route exact path ={`${match.path}`} component= {CollectionOverview}/>
+    <Route path ={`${match.path}/:collectionId`} component={CollectionPage}/>
   </div>
-);
+)};
 
 export default ShopPage;
