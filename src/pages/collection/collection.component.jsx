17b6e6bb30939f5a7530fb98185495a7f6cff6dd
lehon aritonang <lehon_25@yahoo.com>
2019-08-25 22:59:48 +0700

    data normalization collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,9 +7,15 @@ import './collection.styles.scss';
 
 const CollectionPage = ({collection})=>{
     console.log('ini collection',collection)
+    const { title,items } = collection;
     return (
     <div className='collection-page'>
-        <h2>COLLECTION PAGE</h2>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            {
+                items.map(item=><CollectionItem key={item.id} item={item}/>)
+            }
+        </div>
         </div>
 )}
 
